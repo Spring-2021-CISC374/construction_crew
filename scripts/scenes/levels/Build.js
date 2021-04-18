98a22039b0f3c19c4a281aa9d99517195244b7db
Viva Laas <vivalaas@udel.edu>
2021-04-18 16:08:53 -0400

    new animation with a build scene

--- scripts/scenes/levels/Build.js
+++ scripts/scenes/levels/Build.js
@@ -6,27 +6,48 @@ class Build extends Phaser.Scene {
     create() {
         this.background = this.add.image(0, -650, "build_background").setOrigin(0);
 
-        this.build = this.add.sprite(config.width / 2, config.height / 2 + 100, "building");
+        const correct = true;
 
-        this.anims.create({
-            key: "build_anim",
-            frames: this.anims.generateFrameNumbers("building"),
-            frameRate: 2,
-            repeat: 0
-        });
+        var style = { font: "50px Arial", fill: "black", boundsAlignH: "center", boundsAlignV: "middle"};
 
-        this.build.play("build_anim");
+        if(correct){
+            this.build = this.add.sprite(config.width / 3, config.height / 2, "building");
+            this.anims.create({
+                key: "build_anim",
+                frames: this.anims.generateFrameNumbers("building"),
+                frameRate: 2,
+                repeat: 0
+            });
+            this.build.play("build_anim");
+            this.build.scale = 1.5;
 
-        const backButton = this.add.text(840, config.height - 75, 'Main Map!', {
-            font: "50px Arial",
-            fill: '#0f0'
-            })
-        .setInteractive()
-        .on('pointerdown', () => this.updateScene());
+            const message = this.add.text(config.width - 700, config.height / 2 - 100, 'Congradulations!\nYou did it!', style);
+
+            const backButton = this.add.text(config.width - 700, config.height / 2 + 100, 'Main Map!', style)
+            .setInteractive()
+            .on('pointerdown', () => this.updateToMainScene());
+
+        }else{
+
+            this.wrong = this.add.image(config.width / 3, config.height / 2, "wrong");
+
+            const message = this.add.text(config.width - 700, config.height / 2 - 100, 'Incorrect!\nPlease try again!', style);
+
+            const backButton = this.add.text(config.width - 700, config.height / 2 + 100, 'Try Again', style)
+            .setInteractive()
+            .on('pointerdown', () => this.updateScene());
+        }
+        
     }
   
-    updateScene() {
+    updateToMainScene() {
         this.scene.start("MainMap");
     }
+
+    updateScene(){
+        this.scene.start("LevelOne");
+    }
+
+    
 }
   

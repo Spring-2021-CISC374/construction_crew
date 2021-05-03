d3f92cd07569dfc8b81103d5287b19d10f2b6abf
Megha Patel <mpatel@udel.edu>
2021-05-03 18:43:21 -0400

    level 6 instructions done

--- scripts/scenes/levels/LevelSix.js
+++ scripts/scenes/levels/LevelSix.js
@@ -6,36 +6,57 @@ class LevelSix extends Phaser.Scene {
     create() {
       this.background = this.add.image(0, -650, "build_background").setOrigin(0);
 
-      this.add.text(config.width/3, 50, "Welcome to Level Six!", {
-        font: "60px Arial",
-        fill: "black",
-        align: "center"
-      });
-
-      /*
-      this.add.text(105, 120, "To construct your building, schedule the following subcontractors in the proper order into the calendar!", {
+      var speechBubble = this.add.image(0, 0, "speech");
+      speechBubble.scale = 0.8
+      var instructions = this.add.text(0, 0, "Wow! Level 6 already?!" +
+      "\n\nIt looks like you the weather may be clearing up a little! Yay! \n" +
+      "\nBut our project still has a lot of work to be done!" +
+      "\nTo pick up the pace, you can schedule he same subcontractor \non the same day " +
+      "to complete tasks faster!", {
         font: "35px Arial",
         fill: "black",
         align: "center"
       });
+      instructions.setOrigin(0.5, .55)
+      var container = this.add.container(0, 0, [ speechBubble, instructions ]);
+      container.setSize(0, 0)//speechBubble.width/2, speechBubble.height);
+      container.setPosition(config.width/2, config.height/2 - 150);
+      /*
+      const roofer = this.add.image(config.width/4*3 - 230, config.height/2 - 30, "roofer");
+      roofer.scale = .3;
+
+      const painter = this.add.image(config.width/2 + 190, config.height/2 - 110, "painter");
+      painter.scale = 0.05;
+
+      const electrician = this.add.image(config.width/4*3 - 230, config.height/2 - 30, "electrician");
+      electrician.scale = 0.45;
+
+      const plumber = this.add.image(config.width/4*3 - 190, config.height/2 - 260, "plumber");
+      plumber.scale = 0.14;
+
+      const rain = this.add.image(config.width/2 + 25, config.height/2 - 140, "rain")
+      rain.scale = 0.1
+
+      const concrete = this.add.image(config.width/4*2 - 50, config.height/2 - 240, "concrete");
+      concrete.scale = 0.6;
+
+      const farmer = this.add.image(config.width/4*2 + 180, config.height/2 - 245, "farmer");
+      farmer.scale = 0.6;
+
+      const storm = this.add.image(config.width/2 + 120, config.height/2 - 190, "storm")
+      storm.scale = 0.15
       */
-      const roofer = this.add.image(config.width/4 - 200, config.height/2, "roofer");
-      roofer.scale = 1.3;
+      //ANIMATION FOR CHARACTER
 
-      const painter = this.add.image(config.width/2 - 300, config.height/2 - 50, "painter");
-      painter.scale = 0.2;
-
-      const electrician = this.add.image(config.width/2 - 50, config.height/2 - 50, "electritian");
-      electrician.scale = 1.5;
-
-      const plumer = this.add.image(config.width/4*3 - 250, config.height / 2 - 50, "plumer");
-      plumer.scale = 0.5;
-
-      const concrete = this.add.image(config.width/4*3 + 50, config.height / 2 - 50, "concrete");
-      concrete.scale = 3;
-
-      const framer = this.add.image(config.width/4*3 + 350, config.height / 2 - 100, "framers");
-      framer.scale = 3;
+      this.character = this.add.sprite(config.width/8, config.height / 2, "character");
+      this.anims.create({
+        key: "char_anim",
+        frames: this.anims.generateFrameNumbers("character"),
+        frameRate: 4,
+        repeat: -1
+      });
+      this.character.play("char_anim");
+      this.character.scale = 5;
 
       this.createBackToMapButton()
       this.createBeginBuildingButton()

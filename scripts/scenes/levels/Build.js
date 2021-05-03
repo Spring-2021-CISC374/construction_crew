c277c6a9bbf4548da93be50a7496cf5e30bbf1f4
Viva Laas <vivalaas@udel.edu>
2021-05-03 12:32:21 -0400

    scalings adjusted for play screens

--- scripts/scenes/levels/Build.js
+++ scripts/scenes/levels/Build.js
@@ -36,7 +36,6 @@ class Build extends Phaser.Scene {
         }else{
 
             this.wrong = this.add.image(config.width / 3, config.height / 2, "wrong");
-            this.wrong.scale = 4;
 
             const message = this.add.text(config.width - 700, config.height / 2 - 150, 'Incorrect!\nScore: ' + score + '\nPlease try again!\n', style);
 
